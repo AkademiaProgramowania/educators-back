d8f1385540b035bcb2dbfdb576e69e553b0c9074
Maciej Lipinski <maciej.lipinski123@gmail.com>
2023-02-06 18:31:44 +0100

    initial commit

---
+++ src/main/java/com/akademia/educators_back/EducatorsBackApplication.java
@@ -0,0 +1,13 @@
+package com.akademia.educators_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducatorsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducatorsBackApplication.class, args);
+	}
+
+}
