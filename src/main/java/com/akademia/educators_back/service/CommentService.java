8c1ecf8724acdbde0ed59964766ba883b2681973
Maciej Lipinski <maciej.lipinski123@gmail.com>
2023-02-22 23:40:27 +0100

    add body to CommentService

--- src/main/java/com/akademia/educators_back/service/CommentService.java
+++ src/main/java/com/akademia/educators_back/service/CommentService.java
@@ -1,7 +1,24 @@
 package com.akademia.educators_back.service;
 
+import com.akademia.educators_back.entity.CommentEntity;
+import com.akademia.educators_back.repository.CommentRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
+
+    CommentRepository commentRepository;
+
+    public void addCommentToDB(CommentEntity comment) {
+        commentRepository.save(comment);
+    }
+
+    public void deleteProblemFromDB(CommentEntity comment) {commentRepository.delete(comment);
+    }
+
+    public void updateComment(Long id, String newAnswer) {
+        CommentEntity commentEntity = commentRepository.findById(id).orElseThrow(NullPointerException::new);
+        commentEntity.setAnswer(newAnswer);
+        commentRepository.save(commentEntity);
+    }
 }
