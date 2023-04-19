082767024994bdd4239d225d49f99b57f1578092
Maciej Lipinski <maciej.lipinski123@gmail.com>
2023-04-19 18:41:50 +0200

    make single validation method as a private

--- src/main/java/com/akademia/educators_back/valicators/ProblemValidator.java
+++ src/main/java/com/akademia/educators_back/valicators/ProblemValidator.java
@@ -19,52 +19,52 @@ public class ProblemValidator {
     private final int MAX_QUESTION_LENGTH = 1000;
     private ProblemRepository problemRepository;
 
-    public void categoryExistCheck(NewProblemTo newProblemTo) {
+    private void categoryExistCheck(NewProblemTo newProblemTo) {
         if (!problemRepository.existsByCategoryEntity_CategoryName(newProblemTo.getCategoryEntity().getCategoryName())) {
             throw new CategoryDoesNotExistException(newProblemTo.getCategoryEntity().getCategoryName());
         }
     }
 
-    public void questionExistCheck(NewProblemTo newProblemTo) {
+    private void questionExistCheck(NewProblemTo newProblemTo) {
         if (!problemRepository.existsByQuestion(newProblemTo.getQuestion())) {
             throw new ProblemAlreadyExistException(newProblemTo.getQuestion());
         }
     }
 
-    public void titleLengthCheck(NewProblemTo newProblemTo) {
+    private void titleLengthCheck(NewProblemTo newProblemTo) {
         int titleLength = newProblemTo.getTitle().length();
         if (titleLength < MIN_TITLE_LENGTH || titleLength > MAX_TITLE_LENGTH) {
             throw new TextLengthException("Provided title has incorrect length");
         }
     }
 
-    public void questionLengthCheck(NewProblemTo newProblemTo) {
+    private void questionLengthCheck(NewProblemTo newProblemTo) {
         int questionLength = newProblemTo.getQuestion().length();
         if (questionLength < MIN_QUESTION_LENGTH || questionLength > MAX_QUESTION_LENGTH) {
             throw new TextLengthException("Provided question has incorrect length");
         }
     }
 
-    public void categoryExistCheck(ProblemTo problemTo) {
+    private void categoryExistCheck(ProblemTo problemTo) {
         if (!problemRepository.existsByCategoryEntity_CategoryName(problemTo.getCategoryEntity().getCategoryName())) {
             throw new CategoryDoesNotExistException(problemTo.getCategoryEntity().getCategoryName());
         }
     }
 
-    public void questionExistCheck(ProblemTo problemTo) {
+    private void questionExistCheck(ProblemTo problemTo) {
         if (!problemRepository.existsByQuestion(problemTo.getQuestion())) {
             throw new ProblemAlreadyExistException(problemTo.getQuestion());
         }
     }
 
-    public void titleLengthCheck(ProblemTo problemTo) {
+    private void titleLengthCheck(ProblemTo problemTo) {
         int titleLength = problemTo.getTitle().length();
         if (titleLength < MIN_TITLE_LENGTH || titleLength > MAX_TITLE_LENGTH) {
             throw new TextLengthException("Provided title has incorrect length");
         }
     }
 
-    public void questionLengthCheck(ProblemTo problemTo) {
+    private void questionLengthCheck(ProblemTo problemTo) {
         int questionLength = problemTo.getQuestion().length();
         if (questionLength < MIN_QUESTION_LENGTH || questionLength > MAX_QUESTION_LENGTH) {
             throw new TextLengthException("Provided question has incorrect length");
