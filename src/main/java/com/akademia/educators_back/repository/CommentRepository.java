03863696debb6ebea7e14e5dfeaa53c459d4866a
PrzemyslawGromiec <124529312+PrzemyslawGromiec@users.noreply.github.com>
2023-05-11 20:42:32 +0100

    comment_controller_corrected (#20)

--- src/main/java/com/akademia/educators_back/repository/CommentRepository.java
+++ src/main/java/com/akademia/educators_back/repository/CommentRepository.java
@@ -2,6 +2,12 @@ package com.akademia.educators_back.repository;
 
 import com.akademia.educators_back.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+    boolean existsByAnswer(String answer);
+
+    @Query("from CommentEntity p where p.answer =:answer")
+    boolean ifAnswerExists(@Param("answer") String answer);
 }
