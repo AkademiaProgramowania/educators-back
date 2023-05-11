512f83fcdb6a91f10c79594020c1d8391f8669dd
pgromiec <pgromiec@gmail.com>
2023-05-11 16:02:41 +0100

    comment_controller_corrected

---
+++ src/main/java/com/akademia/educators_back/controller/CommentController.java
@@ -0,0 +1,40 @@
+package com.akademia.educators_back.controller;
+
+import com.akademia.educators_back.service.impl.CommentServiceImpl;
+import com.akademia.educators_back.to.CommentTo;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comment")
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class CommentController {
+    @Autowired
+    private CommentServiceImpl commentServiceImpl;
+
+    @GetMapping("/list")
+    public ResponseEntity<List<CommentTo>> getComments() {
+        return ResponseEntity.ok()
+                .body(commentServiceImpl.getComments());
+    }
+
+    @GetMapping("/list/{id}")
+    public ResponseEntity<CommentTo> findCommentById(@PathVariable Long id){
+        CommentTo commentTo = commentServiceImpl.getCommentById(id);
+        return ResponseEntity.ok()
+                .body(commentTo);
+    }
+
+    @PostMapping("/add")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void addComment(@RequestBody CommentTo commentTo) {
+        commentServiceImpl.addComment(commentTo);
+    }
+}
