fa6e4e1793a1f1ff5c79edc96cabc43cc46a4e9d
Maciej Lipinski <maciej.lipinski123@gmail.com>
2023-07-03 09:46:54 +0200

    implement method to add new category

--- src/main/java/com/akademia/educators_back/service/Category.java
+++ src/main/java/com/akademia/educators_back/service/Category.java
@@ -1,8 +1,10 @@
 package com.akademia.educators_back.service;
 
+import com.akademia.educators_back.to.NewCategoryTo;
+
 /**
  * The Category interface responsible for managing Category objects
  */
 public interface Category {
-    void addCategory();
+    void addCategory(NewCategoryTo newCategoryTo);
 }
--- src/main/java/com/akademia/educators_back/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/akademia/educators_back/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,10 @@
 package com.akademia.educators_back.service.impl;
 
+import com.akademia.educators_back.entity.CategoryEntity;
+import com.akademia.educators_back.mapper.CategoryMapper;
+import com.akademia.educators_back.repository.CategoryRepository;
 import com.akademia.educators_back.service.Category;
+import com.akademia.educators_back.to.NewCategoryTo;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +15,17 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class CategoryServiceImpl implements Category {
 
+    private CategoryRepository categoryRepository;
 
+    private CategoryMapper categoryMapper;
+
+    /**
+     * Add new category
+     * @param newCategoryTo New category TO is an object without ID representing a new category
+     */
     @Override
-    public void addCategory() {
-
+    public void addCategory(NewCategoryTo newCategoryTo) {
+        CategoryEntity categoryEntity = categoryMapper.toCategoryEntity(newCategoryTo);
+        categoryRepository.save(categoryEntity);
     }
 }
